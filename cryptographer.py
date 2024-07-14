f0cd0fb0f2a84876840c57e04984929a5acd01c2
AGL <108303210+arthurgalluccilima@users.noreply.github.com>
2024-07-14 18:44:27 -0300

    Update cryptographer.py

--- cryptographer.py
+++ cryptographer.py
@@ -1,62 +1,65 @@
-def cryptographer():
-    import tkinter
-    from cryptography.fernet import Fernet
+from tkinter import *
+from cryptography.fernet import Fernet
 
+
+def cryptographer():
     class App:
         def __init__(self, master):
-            # Left side
-            self.message_entry = tkinter.Text(master, font="Arial 14")
-            self.message_entry.grid(row=0, column=0, sticky="news")
+            # Bottom side
+            self.message_entry = Text(master, font="Arial 14")
+            self.message_entry.grid(row=1, column=0, columnspan=4, sticky="news")
 
-            self.encrypt_button = tkinter.Button(master, text="Encrypt", command=lambda: self.encrypt_message())
-            self.encrypt_button.grid(row=1, column=0, sticky="news")
+            # Toolbar
+            self.left_toolbar = Frame(master)
+            self.left_toolbar.grid(row=0, column=0, columnspan=2, sticky="nsw")
 
-            self.decrypt_button = tkinter.Button(master, text="Decrypt", command=lambda: self.decrypt_message())
-            self.decrypt_button.grid(row=2, column=0, sticky="news")
+            self.right_toolbar = Frame(master)
+            self.right_toolbar.grid(row=0, column=2, columnspan=2, sticky="nse")
 
-            # Right side
-            self.key_entry = tkinter.Text(master, font="Arial 10")
-            self.key_entry.grid(row=0, column=1, sticky="news")
+            self.encrypt_button = Button(self.left_toolbar, text="Encrypt", command=lambda: self.encrypt_message())
+            self.encrypt_button.grid(row=0, column=0, sticky="news")
 
-            self.generate_key_button = tkinter.Button(master, text="Generate key", command=lambda: self.generate_key())
-            self.generate_key_button.grid(row=1, column=1, sticky="news")
+            self.decrypt_button = Button(self.left_toolbar, text="Decrypt", command=lambda: self.decrypt_message())
+            self.decrypt_button.grid(row=0, column=1, sticky="news")
+
+            self.key_entry = Entry(self.right_toolbar, width=50, font="Arial 10")
+            self.key_entry.grid(row=0, column=2, sticky="news")
+
+            self.generate_key_button = Button(self.right_toolbar, text="Generate key", command=lambda: self.generate_key())
+            self.generate_key_button.grid(row=0, column=3, sticky="news")
 
         def generate_key(self):
-            key = Fernet.generate_key()
-            self.key_entry.delete(1.0, tkinter.END)
-            self.key_entry.insert(tkinter.END, key)
+            key = (Fernet.generate_key())
+            self.key_entry.delete(0, END)
+            self.key_entry.insert(END, key)
 
         def encrypt_message(self):
-            key = self.key_entry.get(1.0, tkinter.END)
+            key = self.key_entry.get()
             fer = Fernet(key)
 
-            decrypted_message = self.message_entry.get(1.0, tkinter.END)
+            decrypted_message = self.message_entry.get(1.0, END)
             encrypted_message = fer.encrypt(decrypted_message.encode()).decode()
-            self.message_entry.delete(1.0, tkinter.END)
-            self.message_entry.insert(tkinter.END, encrypted_message)
+            self.message_entry.delete(1.0, END)
+            self.message_entry.insert(END, encrypted_message)
 
         def decrypt_message(self):
-            key = self.key_entry.get(1.0, tkinter.END)
+            key = self.key_entry.get()
             fer = Fernet(key)
 
-            encrypted_message = self.message_entry.get(1.0, tkinter.END)
+            encrypted_message = self.message_entry.get(1.0, END)
             decrypted_message = fer.decrypt(encrypted_message.encode().decode())
-            self.message_entry.delete(1.0, tkinter.END)
-            self.message_entry.insert(tkinter.END, decrypted_message)
+            self.message_entry.delete(1.0, END)
+            self.message_entry.insert(END, decrypted_message)
 
-    root = tkinter.Tk()
+    root = Tk()
     root.title("Cryptographer")
 
-    root.geometry("800x600")
-    root.minsize(800, 600)
-    root.maxsize(800, 600)
+    root.grid_rowconfigure(1, weight=1)
 
     root.grid_columnconfigure(0, weight=1)
     root.grid_columnconfigure(1, weight=1)
-
-    root.grid_rowconfigure(0, weight=1)
-    root.grid_rowconfigure(1, weight=1)
-    root.grid_rowconfigure(2, weight=1)
+    root.grid_columnconfigure(2, weight=1)
+    root.grid_columnconfigure(3, weight=1)
 
     App(root)
     root.mainloop()
